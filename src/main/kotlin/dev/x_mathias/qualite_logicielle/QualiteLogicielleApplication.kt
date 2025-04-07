76498c751d39e13a4971ee948178c9658968911f
mathias <mathias.suchail@yahoo.com>
2025-04-07 11:53:55 +0200

    init spring app

---
+++ src/main/kotlin/dev/x_mathias/qualite_logicielle/QualiteLogicielleApplication.kt
@@ -0,0 +1,11 @@
+package dev.x_mathias.qualite_logicielle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QualiteLogicielleApplication
+
+fun main(args: Array<String>) {
+	runApplication<QualiteLogicielleApplication>(*args)
+}
---
+++ src/test/kotlin/dev/x_mathias/qualite_logicielle/QualiteLogicielleApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.x_mathias.qualite_logicielle
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QualiteLogicielleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
